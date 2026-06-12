dff6e9d58cffb450275e780be70f5a0b6be745ed
Ryan H. <131711662+ryanoutcome20@users.noreply.github.com>
2026-06-12 04:04:56 -0500

    list: Add IDs from 45.32.78.141

--- lua/reflection/lists/servers.lua
+++ lua/reflection/lists/servers.lua
@@ -6,4 +6,7 @@ local PK = "Propkill Server: "
 local HH = "HVH Server: "
 
 return {
+    -- propkill la (45.32.78.141)
+    {"76561199869694615", PK.."propkill la"},
+    {"76561198296481318", PK.."propkill la"},
 }
