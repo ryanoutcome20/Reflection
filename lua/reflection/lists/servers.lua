d02c6b2ab5ea481fde670dc83a74d1362c920fcb
Ryan H. <131711662+ryanoutcome20@users.noreply.github.com>
2026-06-12 04:05:13 -0500

    list: Add IDs from 188.244.191.208

--- lua/reflection/lists/servers.lua
+++ lua/reflection/lists/servers.lua
@@ -9,4 +9,8 @@ return {
     -- propkill la (45.32.78.141)
     {"76561199869694615", PK.."propkill la"},
     {"76561198296481318", PK.."propkill la"},
+
+    -- [ hvhnet ] Casan HVH | 100 tickrate (188.244.191.208)
+    {"76561199160264942", HH.."hvhnet"},
+    {"76561198039814796", HH.."hvhnet"},
 }
